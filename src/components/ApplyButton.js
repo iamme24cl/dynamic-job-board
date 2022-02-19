eba8aa797fd0111ddb3f1b2b9f7e45608bb6d5e5
chandra <iamme24cl@gmail.com>
2022-02-19 14:49:07 -0800

    implemented apply button

---
+++ src/components/ApplyButton.js
@@ -0,0 +1,23 @@
+const ApplyButton = (props) => {
+
+  const styles = {
+    backgroundColor: 'rgb(221, 221, 221)',
+    fontSize: '16px',
+    position: 'absolute',
+    right: '20px',
+    padding: '15px 50px',
+    fontWeight: '800',
+    borderRadius: '12px',
+    border: 'none',
+    cursor: 'pointer',
+    color: 'black'
+  }
+
+  return (
+    <>
+      <a href={`${props.url}`} target="_blank" className="lg-screens-only" style={styles}>Apply</a>
+    </>
+  );
+}
+
+export default ApplyButton;
